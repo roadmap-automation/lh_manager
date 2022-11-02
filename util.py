9646d8fdce45d0ff35ce37034c91c849a459943e
David <david.hoogerheide@nist.gov>
2022-11-02 10:24:55 -0400

    reinstantiation utilities

---
+++ util.py
@@ -0,0 +1,16 @@
+"""Utilities for web server"""
+from typing import Any, obj
+
+def reinstantiate(obj, target_class):
+    """Used in dataclass.__post_init__ to reinstantiate nested dataclasses"""
+    if isinstance(obj, dict):
+        obj = target_class(**obj)
+
+    return obj
+
+def reinstantiate_list(obj_list, target_class):
+    """Used in dataclass.__post_init__ to reinstantiate lists of nested dataclasses"""
+    for i, obj in enumerate(obj_list):
+        obj_list[i] = reinstantiate(obj, target_class)
+
+    return obj_list
