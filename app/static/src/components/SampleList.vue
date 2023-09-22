a52ca7e1b446309320a18e777140b063e3bb6c23
Brian Benjamin Maranville <brian.maranville@nist.gov>
2023-09-22 15:46:53 -0400

    add buttons to run stage and sample

--- app/static/src/components/SampleList.vue
+++ app/static/src/components/SampleList.vue
@@ -4,12 +4,12 @@ import { Modal } from 'bootstrap';
 import { v4 as uuidv4 } from 'uuid';
 import MethodList from './MethodList.vue';
 // import { socket_emit } from '../store.ts';
-import { update_sample, active_item, active_stage, active_method } from '../store';
-import type { SampleStatus, StatusType, Sample } from '../store';
+import { update_sample, run_sample, active_item, active_stage, active_method } from '../store';
+import type { SampleStatus, StatusType, Sample, StageName } from '../store';
 
 const props = defineProps<{
   samples: Sample[],
-  sample_status: SampleStatus
+  sample_status: {[sample_id: string]: {[stage_name in StageName]: SampleStatus}},
 }>();
 
 const emit = defineEmits(['update_sample']);
@@ -47,6 +47,11 @@ async function edit_sample_name(index) {
 function update_sample_name() {
   update_sample(sample_to_edit.value);
   close_modal();
+}
+
+async function run_stage(sample: Sample, stage: StageName[]) {
+  const result = await run_sample(sample, stage);
+  console.log("result of run_stage: ", result);
 }
 
 function close_modal() {
@@ -109,39 +114,40 @@ onMounted(() => {
   })
 });
 
-function get_sample_status_class(sample_id) {
-  const status = props.sample_status[sample_id] ?? {};
+function get_sample_status(sample_id: string): StatusType {
+  const status = props.sample_status[sample_id];
   const stage_status = Object.values(status).map((stage) => stage?.status);
   if (stage_status.every(s => s === 'inactive')) {
-    return '';
-  }
-  else if (stage_status.some((s) => s === 'pending')) {
-    return 'text-primary';
+    return 'inactive';
   }
   else if (stage_status.some((s) => s === 'active')) {
-    return 'text-success';
+    return 'active';
+  }
+  else if (stage_status.some((s) => s === 'pending')) {
+    return 'pending';
   }
   else if (stage_status.every((s) => s === 'completed')) {
-    return 'text-danger';
+    return 'completed';
   }
-  else if (stage_status.some((s) => s === 'completed')) {
-    return 'text-warning';
+  else if (stage_status.some((s) => s === 'completed') && stage_status.some((s) => s === 'inactive')) {
+    return 'partially complete';
   }
   else {
     console.warn(`unexpected status: ${JSON.stringify(status)}`);
     return null;
   }
-
 }
 
 const status_by_id = computed(() => {
   return Object.fromEntries(props.samples.map((s) => {
-    return [s.id, get_sample_status_class(s.id)];
+    return [s.id, get_sample_status(s.id)];
   }));
 })
 
-const status_class_map = {
-  'pending': '',
+const status_class_map: {[status in StatusType]: string} = {
+  'inactive': '',
+  'partially complete': 'text-warning',
+  'pending': 'text-secondary',
   'active': 'text-success',
   'completed': 'text-danger',
 }
@@ -154,18 +160,32 @@ const status_class_map = {
     <div class="accordion-item" v-for="(sample, sindex) of samples" :key="sample.id">
       <div class="accordion-header">
         <button class="accordion-button p-1"
-          :class="{ collapsed: sindex !== active_item, [status_by_id[sample.id]]: true }" type="button"
+          :class="{ collapsed: sindex !== active_item, [status_class_map[status_by_id[sample.id]]]: true }" type="button"
           @click="toggleItem(sindex)" :aria-expanded="sindex === active_item">
           <span class="fw-bold align-middle px-2"> {{ sample.name }} </span>
           <span class="align-middle px-2"> {{ sample.description }}</span>
           <button type="button" class="btn-close btn-sm align-middle edit" aria-label="Edit Name or Description"
             @click.stop="edit_sample_name(sindex)"></button>
+            <button 
+              v-if="status_by_id[sample.id] === 'inactive'"
+              type="button"
+              class="btn-close btn-sm align-middle start"
+              aria-label="Run stage"
+              @click.stop="run_stage(sample, ['prep', 'inject'])">
+            </button>
         </button>
       </div>
       <div class="accordion-collapse collapse" :class="{ show: sindex === active_item }">
         <div class="accordion-body py-0">
           <div v-for="(stage, stage_name) of sample.stages">
-            <h6 :class="status_class_map[sample_status?.[sample.id]?.[stage_name]?.status ?? 'pending']">{{ stage_name }}:
+            <h6 :class="status_class_map[sample_status?.[sample.id]?.[stage_name]?.status ?? 'inactive']">{{ stage_name }}:
+              <button 
+                v-if="sample_status?.[sample.id]?.[stage_name]?.status === 'inactive'"
+                type="button"
+                class="btn-close btn-sm align-middle start"
+                aria-label="Run stage"
+                @click.stop="run_stage(sample, [stage_name])">
+              </button>
             </h6>
             <MethodList :methods="stage.methods" :status="sample_status?.[sample.id]?.prep" :collapsed="!(sindex === active_item && stage_name == active_stage)"
               :active_item="(stage_name === active_stage) ? active_method : null"
@@ -211,5 +231,9 @@ const status_class_map = {
 <style>
 .btn-close.edit {
   background-image: url("data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pencil-square' viewBox='0 0 16 16'><path d='M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z'/><path fill-rule='evenodd' d='M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z'/></svg>")
+}
+
+.btn-close.start {
+  background-image: url('data:image/svg+xml,<svg class="svg-icon" style="width: 1em; height: 1em;vertical-align: middle;fill: currentColor;overflow: hidden;" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M212.992 526.336 212.992 526.336 212.992 526.336 215.04 526.336 212.992 526.336Z"  /><path d="M817.152 202.752 817.152 202.752C737.28 122.88 628.736 75.776 509.952 75.776c-118.784 0-229.376 49.152-307.2 126.976l0 0c-77.824 77.824-126.976 186.368-126.976 307.2 0 118.784 49.152 229.376 126.976 307.2 77.824 79.872 188.416 126.976 307.2 126.976 120.832 0 229.376-49.152 307.2-126.976 79.872-77.824 126.976-186.368 126.976-307.2C946.176 389.12 897.024 280.576 817.152 202.752zM770.048 770.048c-65.536 65.536-157.696 108.544-260.096 108.544-102.4 0-194.56-40.96-260.096-108.544C184.32 704.512 141.312 612.352 141.312 509.952s40.96-194.56 108.544-260.096C317.44 184.32 409.6 141.312 509.952 141.312c100.352 0 192.512 40.96 258.048 106.496l2.048 2.048c65.536 65.536 108.544 157.696 108.544 260.096S837.632 704.512 770.048 770.048z"  /><path d="M385.024 249.856l202.752 116.736 202.752 116.736c14.336 8.192 18.432 26.624 10.24 40.96-4.096 4.096-8.192 8.192-12.288 12.288l-202.752 116.736 0 0-202.752 116.736c-14.336 8.192-32.768 4.096-40.96-10.24-2.048-6.144-4.096-10.24-4.096-16.384L337.92 509.952l0 0L337.92 274.432c0-16.384 12.288-28.672 28.672-28.672C374.784 245.76 380.928 247.808 385.024 249.856L385.024 249.856z"  /></svg>');
 }
 </style>
