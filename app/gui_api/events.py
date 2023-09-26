7f30269164d9bb7fd3dc1cb3d0b472599a401806
hoogerheide <david.hoogerheide@nist.gov>
2023-09-26 15:49:54 -0400

    incorporate persisting into socketio events

--- app/gui_api/events.py
+++ app/gui_api/events.py
@@ -1,10 +1,28 @@
 from sio import socketio
+import os
+import json
+from liquid_handler.state import layout, samples
+from dataclasses import asdict
+
+LAYOUT_LOG = 'persistent_state/layout.json'
+SAMPLES_LOG = 'persistent_state/samples.json'
+
+@socketio.on('update_layout')
+def layout_update_received():
+    #print('dumping layout')
+    json.dump(asdict(layout), open(LAYOUT_LOG, 'w'))
+
+@socketio.on('update_sample_status')
+def samples_update_received():
+    #print('dumping samples')
+    json.dump(asdict(samples), open(SAMPLES_LOG, 'w'))
 
 def trigger_layout_update(f):
     """Decorator that announces that layout has changed"""
     def wrap(*args, **kwargs):
         ret_val = f(*args, **kwargs)
         socketio.emit('update_layout', {'msg': 'update_layout'}, include_self=True)
+        layout_update_received()
         return ret_val
     wrap.__name__ = f.__name__
     return wrap
@@ -14,6 +32,7 @@ def trigger_samples_update(f):
     def wrap(*args, **kwargs):
         ret_val = f(*args, **kwargs)
         socketio.emit('update_samples', {'msg': 'update_samples'}, include_self=True)
+        samples_update_received()
         return ret_val
     wrap.__name__ = f.__name__
     return wrap
@@ -23,18 +42,8 @@ def trigger_sample_status_update(f):
     def wrap(*args, **kwargs):
         ret_val = f(*args, **kwargs)
         socketio.emit('update_sample_status', {'msg': 'update_sample_status'}, include_self=True)
+        samples_update_received()
         return ret_val
     wrap.__name__ = f.__name__
     return wrap
 
-@socketio.on('layout_update_received')
-def layout_update_received():
-    print('received layout update')
-
-@socketio.on('samples_update_received')
-def samples_update_received():
-    print('received samples update')
-
-@socketio.on('sample_status_update_received')
-def sample_status_update_received():
-    print('received sample status update')
