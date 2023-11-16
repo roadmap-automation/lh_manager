90f6e062c468622425201d2a9fc84eb7b7b05858
bbm <brian.maranville@nist.gov>
2023-11-16 15:58:06 -0500

    add archive button to samples where status is 'inactive'

--- liquid_handler_api/static/src/components/SampleList.vue
+++ liquid_handler_api/static/src/components/SampleList.vue
@@ -4,7 +4,7 @@ import Modal from 'bootstrap/js/src/modal';
 import { v4 as uuidv4 } from 'uuid';
 import MethodList from './MethodList.vue';
 // import { socket_emit } from '../store.ts';
-import { samples, sample_status, update_sample, run_sample, active_sample_index, active_stage, active_method_index } from '../store';
+import { samples, sample_status, update_sample, run_sample, active_sample_index, active_stage, active_method_index, archive_and_remove_sample } from '../store';
 import type { SampleStatus, SampleStatusMap, StatusType, Sample, StageName } from '../store';
 
 const emit = defineEmits(['update_sample']);
@@ -39,6 +39,12 @@ async function edit_sample_name(index) {
   modal_title.value = "Edit Sample Name and Description";
   sample_to_edit.value = { id, name, description };
   modal.value?.show();
+}
+
+async function archive_sample(index) {
+  const s = samples[index];
+  const result = await archive_and_remove_sample(s);
+  return result;
 }
 
 function update_sample_name() {
@@ -99,11 +105,18 @@ const status_class_map: {[status in StatusType]: string} = {
           <button type="button" class="btn-close btn-sm align-middle edit" aria-label="Edit Name or Description"
             @click.stop="edit_sample_name(sindex)"></button>
             <button 
-              v-if="(sample_status[sample.id]?.status ?? 'inactive') === 'inactive'"
+              v-if="(sample_status?.[sample.id]?.status ?? 'inactive') === 'inactive'"
               type="button"
               class="btn-close btn-sm align-middle start"
-              aria-label="Run stage"
+              aria-label="Run stages"
               @click.stop="run_stage(sample, ['prep', 'inject'])">
+            </button>
+            <button
+              v-if="(sample_status[sample.id]?.status ?? 'inactive') === 'inactive'"
+              type="button"
+              class="btn-close btn-sm align-middle archive"
+              aria-label="Archive sample"
+              @click.stop="archive_and_remove_sample(sample.id)">
             </button>
         </button>
       </div>
@@ -123,9 +136,6 @@ const status_class_map: {[status in StatusType]: string} = {
               :sample_id="sample.id"
               :stage_name="stage_name"
               :methods="stage.methods"
-              @add_method="(m) => add_method(m, sample, stage_name)"
-              @set_location="(i, n, l) => set_location(i, n, l, sample, stage_name)"
-              @set_active="(index) => toggle_active_method(stage_name, index)"
               @update_method="(index, field_name, field_value) => update_method(sample, stage_name, index, field_name, field_value)" />
           </div>
         </div>
@@ -169,5 +179,9 @@ const status_class_map: {[status in StatusType]: string} = {
 
 .btn-close.start {
   background-image: url('data:image/svg+xml,<svg class="svg-icon" style="width: 1em; height: 1em;vertical-align: middle;fill: currentColor;overflow: hidden;" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M212.992 526.336 212.992 526.336 212.992 526.336 215.04 526.336 212.992 526.336Z"  /><path d="M817.152 202.752 817.152 202.752C737.28 122.88 628.736 75.776 509.952 75.776c-118.784 0-229.376 49.152-307.2 126.976l0 0c-77.824 77.824-126.976 186.368-126.976 307.2 0 118.784 49.152 229.376 126.976 307.2 77.824 79.872 188.416 126.976 307.2 126.976 120.832 0 229.376-49.152 307.2-126.976 79.872-77.824 126.976-186.368 126.976-307.2C946.176 389.12 897.024 280.576 817.152 202.752zM770.048 770.048c-65.536 65.536-157.696 108.544-260.096 108.544-102.4 0-194.56-40.96-260.096-108.544C184.32 704.512 141.312 612.352 141.312 509.952s40.96-194.56 108.544-260.096C317.44 184.32 409.6 141.312 509.952 141.312c100.352 0 192.512 40.96 258.048 106.496l2.048 2.048c65.536 65.536 108.544 157.696 108.544 260.096S837.632 704.512 770.048 770.048z"  /><path d="M385.024 249.856l202.752 116.736 202.752 116.736c14.336 8.192 18.432 26.624 10.24 40.96-4.096 4.096-8.192 8.192-12.288 12.288l-202.752 116.736 0 0-202.752 116.736c-14.336 8.192-32.768 4.096-40.96-10.24-2.048-6.144-4.096-10.24-4.096-16.384L337.92 509.952l0 0L337.92 274.432c0-16.384 12.288-28.672 28.672-28.672C374.784 245.76 380.928 247.808 385.024 249.856L385.024 249.856z"  /></svg>');
+}
+
+.btn-close.archive {
+  background-image: url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-in-down" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M3.5 6a.5.5 0 0 0-.5.5v8a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5v-8a.5.5 0 0 0-.5-.5h-2a.5.5 0 0 1 0-1h2A1.5 1.5 0 0 1 14 6.5v8a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 14.5v-8A1.5 1.5 0 0 1 3.5 5h2a.5.5 0 0 1 0 1h-2z"/><path fill-rule="evenodd" d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/></svg>')
 }
 </style>
