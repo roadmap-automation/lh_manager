69dd347b84790cfdedd114394b548952c7eb629d
hoogerheide <david.hoogerheide@nist.gov>
2024-01-03 10:33:33 -0500

    Serial Dilution with Injection

--- liquid_handler_api/liquid_handler/dilution.py
+++ liquid_handler_api/liquid_handler/dilution.py
@@ -1,15 +1,11 @@
 from typing import List, Tuple, Literal
-from copy import copy, deepcopy
-import numpy as np
-from scipy.optimize import nnls
+from copy import copy
 from dataclasses import field
 from pydantic.v1.dataclasses import dataclass
 
-from .bedlayout import Solute, Solvent, Composition, LHBedLayout, Well, WellLocation
-from .layoutmap import Zone, LayoutWell2ZoneWell
-from .samplelist import example_sample_list, StageName
+from .bedlayout import LHBedLayout, WellLocation
 from .methods import MethodContainer, MethodsType, TransferWithRinse, MixWithRinse, \
-            TransferMethod, MixMethod, register, method_manager
+            TransferMethod, MixMethod, InjectMethod, InjectWithRinse, register, method_manager
 
 @register
 @dataclass
@@ -25,7 +21,9 @@ class SerialDilution(MethodContainer):
     number_of_dilutions: int = 10
     initial_dilution_factor: float = 1.0
     dilution_factor: float = 2.0
-    volume: float = 1.0
+    min_volume: float = 1.0
+    max_volume: float = 1.0
+    extra_volume: float = 0.15
     transfer_template: TransferMethod = field(default_factory=TransferWithRinse)
     mix_template: MixMethod = field(default_factory=MixWithRinse)
 
@@ -41,12 +39,6 @@ class SerialDilution(MethodContainer):
         success = True
         # if number_of_dilutions is infinite, total volume required per well is up to
         # self.target_volume * (1 + 1 / (dilution_factor - 1)).
-        max_volume_required = self.volume * (1 + 1. / (self.dilution_factor - 1))
-
-        rack_max_volume = layout.racks[self.first_target_well.rack_id].max_volume
-        if max_volume_required > rack_max_volume:
-            success = False
-            raise Warning(f'Maximum volume required for serial dilutions is {max_volume_required}, only {rack_max_volume} is available in rack id {self.first_target_well.rack_id}')
 
         # 1. Calculate total volume required in each well
 
@@ -55,109 +47,134 @@ class SerialDilution(MethodContainer):
 
         # total required to make next dilution (last one is just the target_volume) and leave
         # the appropriate target volume behind
-        total_volumes = [self.volume]
         previous_dilution_volume = 0.0
+        previous_target_volume = self.max_volume + self.extra_volume
+        injection_volumes = [previous_target_volume - self.extra_volume]
+        total_volumes = [previous_target_volume + previous_dilution_volume]
 
         # start from the last one and work backwards
         for df in dilution_factors[::-1]:
-            previous_dilution_volume = (self.volume + previous_dilution_volume) / df
-            total_volumes.append(self.volume + previous_dilution_volume)
+            #print(df, previous_target_volume, previous_dilution_volume, total_volumes[-1])
+            previous_dilution_volume = (previous_target_volume + previous_dilution_volume) / df
+            previous_target_volume = max(previous_target_volume / df, self.min_volume + self.extra_volume)
+            total_volumes.append(previous_target_volume + previous_dilution_volume)
+            injection_volumes.append(previous_target_volume - self.extra_volume)
 
         total_volumes = total_volumes[::-1]
+        injection_volumes = injection_volumes[::-1]
 
         # 2. Calculate diluent fractions for each well
         diluent_fractions = [1.0 - 1.0 / self.initial_dilution_factor] + [1.0 - 1.0 / self.dilution_factor] * (self.number_of_dilutions - 1)
 
-        return total_volumes, diluent_fractions, success
+        return total_volumes, diluent_fractions, injection_volumes, success
 
-    def get_methods(self, layout: LHBedLayout) -> List[MethodsType]:
-        """Overwrites base class method to dynamically create list of methods
-        """
+    def _get_transfermix_methods(self, layout: LHBedLayout) -> Tuple[List[List[MethodsType]], List[List[MethodsType]]]:
 
-        methods = []
-        total_volumes, diluent_fractions, success = self._find_dilution_volumes(layout)
+        transfer_methods = []
+        mix_methods = []
+        target_wells = []
+        total_volumes, diluent_fractions, injection_volumes, success = self._find_dilution_volumes(layout)
 
         if success:
             target_well = copy(self.first_target_well)
             sample_well = copy(self.sample_source)
             for total_volume, diluent_fraction in zip(total_volumes, diluent_fractions):
+
+                itransfer_methods = []
+                imix_methods = []
                 
                 # diluent transfer
                 new_transfer = copy(self.transfer_template)
                 new_transfer.Source = self.diluent_source
                 new_transfer.Target = target_well
                 new_transfer.Volume = total_volume * diluent_fraction
-                methods.append(new_transfer)
+                itransfer_methods.append(new_transfer)
 
                 # sample transfer
                 new_transfer = copy(self.transfer_template)
                 new_transfer.Source = sample_well
                 new_transfer.Target = target_well
                 new_transfer.Volume = total_volume * (1.0 - diluent_fraction)
-                methods.append(new_transfer)
+                itransfer_methods.append(new_transfer)
 
                 # mix
-                mix_volume = min(0.9 * total_volume, total_volume - 0.15)
+                mix_volume = min(0.9 * total_volume, total_volume - self.extra_volume)
                 mix_volume = min(total_volume, mix_volume)
 
                 new_mix = copy(self.mix_template)
                 new_mix.Target = target_well
                 new_mix.Volume = mix_volume
-                methods.append(new_mix)
+                imix_methods.append(new_mix)
+
+                # add transfer and mix methods to respective lists
+                transfer_methods.append(itransfer_methods)
+                mix_methods.append(imix_methods)
+                target_wells.append(target_well)
 
                 # update sample (now previous target) and target (next well)
                 sample_well = copy(target_well)
                 target_well = WellLocation(target_well.rack_id, target_well.well_number + 1)
 
+        return transfer_methods, mix_methods, target_wells, injection_volumes, success
+
+    def get_methods(self, layout: LHBedLayout) -> List[MethodsType]:
+        """Overwrites base class method to dynamically create list of methods
+        """
+
+        methods = []
+        transfer_methods, mix_methods, _, _, success = self._get_transfermix_methods(layout)
+
+        if success:
+
+            nested_methods = [tm + mm for tm, mm in zip(transfer_methods, mix_methods)]
+            methods = [item for mlist in nested_methods for item in mlist]
+
         return methods
+
 
 @register
 @dataclass
-class SerialDilutionVariableVolume(SerialDilution):
+class SerialDilutionInject(SerialDilution):
 
-    # Defined from BaseMethod
-    # complete: bool
-    method_name: Literal['SerialDilutionVariableVolume'] = 'SerialDilutionVariableVolume'
-    display_name: Literal['Serial DilutionVariableVolume'] = 'Serial DilutionVariableVolume'
-    max_volume: float = 1.0
+    method_name: Literal['SerialDilutionInject'] = 'SerialDilutionInject'
+    display_name: Literal['Serial Dilution with Injection'] = 'Serial Dilution with Injection'
+    inject_template: InjectMethod = field(default_factory=InjectWithRinse)
+    inject_immediately: bool = True
 
     def __post_init__(self):
-        for attr_name in ('mix_template', 'transfer_template'):
+        for attr_name in ('mix_template', 'transfer_template', 'inject_template'):
             attr = getattr(self, attr_name)
             if isinstance(attr, dict):
                 setattr(self, attr_name, method_manager.get_method_by_name(attr['method_name'])(**attr))
 
-    def _find_dilution_volumes(self,
-                              layout: LHBedLayout):
-        
-        success = True
-        # if number_of_dilutions is infinite, total volume required per well is up to
-        # self.target_volume * (1 + 1 / (dilution_factor - 1)).
 
-        # 1. Calculate total volume required in each well
+    def get_methods(self, layout: LHBedLayout) -> List[MethodsType]:
+        """Overwrites base class method to dynamically create list of methods
+        """
 
-        # dilution factors
-        dilution_factors = [self.dilution_factor] * (self.number_of_dilutions - 1)
+        methods = []
+        transfer_methods, mix_methods, target_wells, injection_volumes, success = self._get_transfermix_methods(layout)
 
-        # total required to make next dilution (last one is just the target_volume) and leave
-        # the appropriate target volume behind
-        total_volumes = [self.max_volume]
-        previous_dilution_volume = 0.0
-        previous_target_volume = self.max_volume
+        if success:
+            inject_methods = []
+            for target_well, volume in zip(target_wells, injection_volumes):
+                new_inject = copy(self.inject_template)
+                new_inject.Source = target_well
+                new_inject.Volume = volume
+                inject_methods.append([new_inject])
 
-        # start from the last one and work backwards
-        for df in dilution_factors[::-1]:
-            #print(df, previous_target_volume, previous_dilution_volume, total_volumes[-1])
-            previous_dilution_volume = (previous_target_volume + previous_dilution_volume) / df
-            previous_target_volume = max(previous_target_volume / df, self.volume)
-            total_volumes.append(previous_target_volume + previous_dilution_volume)
+            # if inject_immediately, interleave inject methods with transfer and mix methods;
+            #    otherwise lump them all at the end.
+            if self.inject_immediately:
+                nested_methods = [tm + mm + im for tm, mm, im in zip(transfer_methods, mix_methods, inject_methods)]
+                
+            else:
+                nested_methods = [tm + mm for tm, mm in zip(transfer_methods, mix_methods)] + inject_methods
 
-        total_volumes = total_volumes[::-1]
 
-        # 2. Calculate diluent fractions for each well
-        diluent_fractions = [1.0 - 1.0 / self.initial_dilution_factor] + [1.0 - 1.0 / self.dilution_factor] * (self.number_of_dilutions - 1)
+            methods = [item for mlist in nested_methods for item in mlist]
 
-        return total_volumes, diluent_fractions, success
+        return methods
 
 if __name__ == '__main__':
 
