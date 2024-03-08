011b2b546d3c65936340a6becaeb39a45eae94f0
David P. Hoogerheide <david.hoogerheide@nist.gov>
2024-03-08 17:11:31 -0500

    create standalone LH interface, update LH endpoints

--- liquid_handler_api/lh_api/endpoints.py
+++ liquid_handler_api/lh_api/endpoints.py
@@ -1,53 +1,113 @@
 """Gilson Trilution LH 4.0 Endpoints"""
+from dataclasses import asdict
 from flask import make_response, Response, request
 
-from ..liquid_handler.state import samples, layout
-from ..liquid_handler.lhqueue import LHqueue
-from ..liquid_handler.samplelist import SampleStatus, Sample
-from ..gui_api.events import trigger_layout_update, trigger_run_queue_update, \
-                            trigger_sample_status_update, trigger_samples_update
+from ..liquid_handler.lhinterface import lh_interface, LHJob, ValidationStatus, ResultStatus, LHJobHistory, InterfaceStatus
+from ..sio import socketio
 
 from . import lh_blueprint
 
+def broadcast_job_update(job: LHJob) -> None:
+    """Sends signal with job ID. Clients can access /LH/GetJob to 
+        get the updated job information.
+
+    Args:
+        job (LHJob): job to be updated
+    """
+
+    socketio.emit('job_updated', {'job_id': job.id}, include_self=True)
+
+@lh_blueprint.route('/LH/GetJob/<job_id>', methods=['GET'])
+def GetJob(job_id: str) -> Response:
+    """Gets job object from database"""
+
+    with LHJobHistory() as history:
+        job = history.get_job_by_uuid(job_id)
+
+    if job is not None:
+        return make_response({'success': asdict(job)})
+    else:
+        return make_response({'error': f'job {job_id} does not exist'})
+
+@lh_blueprint.route('/LH/SubmitJob/', methods=['POST'])
+def SubmitJob() -> Response:
+    """Submits LHJob to server. Data format should just be a single
+        serialized LHJob object"""
+
+    # Ensure lh_interface is ready to receive a job
+    if lh_interface.get_status() != InterfaceStatus.UP:
+        return make_response({'error': 'job rejected, LH interface busy'})
+
+    # attempt deserialization, raise error if it fails
+    data = request.get_json(force=True)
+
+    try:
+        job = LHJob(**data)
+    except:
+        return make_response({'error': 'job rejected, cannot be deserialized'})
+
+    # activate the job
+    lh_interface.activate_job(job)
+    broadcast_job_update(job)
+
+    return make_response({'success': 'job accepted'})
+
 @lh_blueprint.route('/LH/GetListofSampleLists/', methods=['GET'])
 def GetListofSampleLists() -> Response:
-    sample_list = [sample.toSampleList(stage_name, layout, entry=True) for sample in samples.samples for stage_name in sample.stages if sample.stages[stage_name].status==SampleStatus.ACTIVE]
+    """Gets list of sample lists for Gilson LH
+
+    Returns:
+        Response: format {'sampleLists': sample_list}
+    """
+
+    job: LHJob | None = lh_interface.get_active_job()
+    sample_list = [] if job is None else [job.get_samplelist(listonly=True)]
 
     return make_response({'sampleLists': sample_list}, 200)
 
 @lh_blueprint.route('/LH/GetSampleList/<sample_list_id>', methods=['GET'])
-def GetSampleList(sample_list_id) -> Response:
-    sample, stage_name = samples.getSampleStagebyLH_ID(int(sample_list_id))
-    sampleList = sample.toSampleList(stage_name, layout) if sample is not None and stage_name is not None else []
+def GetSampleList(sample_list_id: str) -> Response:
+    """Gets specific sample list by ID
 
-    return make_response({'sampleList': sampleList}, 200)
+    Args:
+        sample_list_id (str): string representation of sample list ID
+
+    Returns:
+        Response: sample list, or error if ID does not match active sample
+    """
+    job = lh_interface.get_active_job()
+    if job is None:
+        return make_response({'error': 'no active jobs'}, 200)
+    if int(sample_list_id) != job.LH_id:
+        return make_response({'error': f'requested job ID {sample_list_id} does not match active job ID {job.LH_id}'}, 200)
+
+    sample_list = [job.get_samplelist(listonly=False)]
+
+    return make_response({'sampleLists': sample_list}, 200)
 
 @lh_blueprint.route('/LH/PutSampleListValidation/<sample_list_id>', methods=['POST'])
 def PutSampleListValidation(sample_list_id):
     data = request.get_json(force=True)
 
-    # check validation (SUCCESS or ERROR)
-    validation = data['validation']['validationType']
+    job = lh_interface.get_active_job()
+
+    if job is None:
+        return make_response({'error': 'no active jobs'}, 200)
+    if int(sample_list_id) != job.LH_id:
+        return make_response({'error': f'validation job ID {sample_list_id} does not match active job ID {job.LH_id}'}, 200)
+
+    job.validation = data
+    lh_interface.update_job(job)
+    broadcast_job_update(job)
+
     error = None
-    
-    # TODO: Actual error handling flow, sample activation/inactivation methods
-    if not validation == 'SUCCESS':
+    if job.get_validation_status() != ValidationStatus.SUCCESS:
         error = f'Error in validation. Full message: ' + data['validation']['message']
-        sample, stage_name = samples.getSampleStagebyLH_ID(int(sample_list_id))
-        sample.stages[stage_name].status = SampleStatus.INACTIVE
-        trigger_sample_status_update(lambda: 1)
+        lh_interface.deactivate()
 
-    return make_response({sample_list_id: validation, 'error': error}, 200)
-
-@trigger_samples_update
-def _delete_sample(sample: Sample) -> None:
-    """Deletes sample with decorator that triggers update"""
-    samples.deleteSample(sample)
+    return make_response({sample_list_id: job.get_validation_status(), 'error': error}, 200)
 
 @lh_blueprint.route('/LH/PutSampleData/', methods=['POST'])
-@trigger_sample_status_update
-@trigger_layout_update
-@trigger_run_queue_update
 def PutSampleData():
     data = request.get_json(force=True)
     assert isinstance(data, dict)
@@ -56,44 +116,23 @@ def PutSampleData():
     sample_id = int(data['sampleData']['runData'][0]['sampleListID'])
     method_number = int(data['sampleData']['runData'][0]['iteration']) - 1
     method_name = data['sampleData']['runData'][0]['methodName']
-    #print(sample_id, method_number, method_name)
 
-    # check that run was successful
-    if any([('completed successfully' in notification) for notification in data['sampleData']['resultNotifications']['notifications'].values()]):
-        # find relevant sample by ID
-        sample, stage_name = samples.getSampleStagebyLH_ID(sample_id)
-        assert sample is not None and stage_name is not None, "unknown sample"
-        methodlist = sample.stages[stage_name]
-        #print(methodlist.__dict__)
-        method = methodlist.run_methods[method_number]
+    job = lh_interface.get_active_job()
 
-        # double check that correct method is being referenced
-        assert method_name == method.method_name, f'Wrong method name {method_name} in result, expected {method.method_name}, full output {data}'
+    if job is None:
+        return make_response({'error': 'no active jobs'}, 200)
+    if sample_id != job.LH_id:
+        return make_response({'error': f'PutSampleData job ID {sample_id} does not match active job ID {job.LH_id}'}, 200)
+    if job.samplelist['columns'][method_number]['METHODNAME'] != method_name
+        return make_response({'error': f'PutSampleData method name {method_name} does not match corresponding method name {job.samplelist['columns']['METHODNAME']}'}, 200)
 
-        # mark method complete
-        methodlist.run_methods_complete[method_number] = True
+    job.results.append(data)
+    lh_interface.update_job(job)
+    broadcast_job_update(job)
 
-        # Change layout state:
-        method.execute(layout)
-
-        # Change sample state if the method does this:
-        new_state = method.new_sample_composition(layout)
-        if len(new_state):
-            sample.current_contents = new_state
-
-        # if all methods complete, change status of method list to completed, flag LH as no longer busy, and run the next queue item
-        if methodlist.get_method_completion():
-            methodlist.status = SampleStatus.COMPLETED
-
-            # activate next queue item
-            LHqueue.active_sample = None
-            LHqueue.run_next()
-
-        # archive the sample and remove it from active samples if all stages are complete
-        samples.archiveSample(sample)
-        if sample.get_status() == SampleStatus.COMPLETED:
-            _delete_sample(sample)
-
-    # TODO: ELSE: Throw error
+    error = None
+    if job.get_result_status() == ResultStatus.FAIL:
+        error = f'Error in results. Full message: ' + data
+        lh_interface.deactivate()
 
     return make_response({'data': data}, 200)
---
+++ liquid_handler_api/liquid_handler/lhinterface.py
@@ -0,0 +1,249 @@
+import os
+import copy
+import json
+import sqlite3
+
+from enum import Enum
+from pathlib import Path
+from typing import List, Tuple
+from dataclasses import asdict, field
+from pydantic.v1.dataclasses import dataclass
+
+LH_JOB_HISTORY = Path(__file__).parent.parent.parent / 'persistent_state' / 'lh_jobs.sqlite'
+
+class ValidationStatus(str, Enum):
+    UNVALIDATED = 'unvalidated',
+    SUCCESS = 'success',
+    FAIL = 'failed'
+
+class ResultStatus(str, Enum):
+    EMPTY = 'empty',
+    INCOMPLETE = 'incomplete',
+    SUCCESS = 'success',
+    FAIL = 'failed'
+
+class InterfaceStatus(str, Enum):
+    UP = 'up'
+    BUSY = 'busy'
+    DOWN = 'down'
+    ERROR = 'error'
+
+@dataclass
+class LHJob:
+    """Container for a single liquid handler sample list"""
+    id: str
+    LH_id: int
+    samplelist: dict
+    validation: dict = field(default_factory=dict)
+    results: list = field(default_factory=list)
+    
+    def get_validation_status(self) -> Tuple[ValidationStatus, dict | None]:
+        """Returns true if validation exists """
+
+        if not len(self.validation):
+            return ValidationStatus.UNVALIDATED, None
+        
+        if self.validation['validation']['validationType'] == 'SUCCESS':
+            return ValidationStatus.SUCCESS, None
+        else:
+            return ValidationStatus.FAIL, self.validation
+        
+    def get_result_status(self) -> ResultStatus:
+        # if no results
+        if not len(self.results):
+            return ResultStatus.EMPTY
+        
+        # check for any failures in existing results
+        if any([('completed successfully' not in notification)
+                for result in self.results
+                for notification in result['sampleData']['resultNotifications']['notifications'].values()]):
+            
+            return ResultStatus.FAIL
+
+        # check for incomplete results (should be one per method in columns)
+        if len(self.results) < len(self.samplelist['columns']):
+            return ResultStatus.INCOMPLETE
+        
+        # if all checks pass, we were successful
+        return ResultStatus.SUCCESS
+    
+    def get_samplelist(self, listonly=False) -> dict:
+        """Gets the sample list formatted for Gilson LH.
+        
+        Args:
+            listonly (bool, optional): Only return header information. Defaults to False.
+
+        Returns:
+            dict: sample list prepared for Gilson LH
+        """
+
+        samplelist = copy.copy(self.samplelist)
+        if listonly:
+            samplelist['columns'] = None
+
+        return samplelist
+
+class LHJobHistory:
+    table_name = 'lh_job_record'
+    table_definition = f"""\
+        CREATE TABLE IF NOT EXISTS {table_name}(
+            uuid TEXT PRIMARY KEY,
+            LH_id INTEGER,
+            job JSON,
+            timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        ) WITHOUT ROWID;"""
+    
+    def __init__(self, database_path: str = LH_JOB_HISTORY) -> None:
+        self.db_path = database_path
+        self.db = None
+
+    def __enter__(self):
+        self.open()
+        return self
+    
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    def open(self) -> None:
+        db_exists = os.path.exists(self.db_path)
+        self.db = sqlite3.connect(self.db_path)
+        if not db_exists:
+            self.db.execute(self.table_definition)
+
+    def close(self) -> None:
+        self.db.close()
+
+    def smart_insert(self, job: LHJob) -> None:
+        """Inserts or, if job already exists, updates a liquid handler job. Uses id as unique identifier
+
+        Args:
+            job (LHJob): liquid handler job to update or insert into the history
+        """
+
+        res = self.db.execute(f"""\
+            INSERT INTO {self.table_name}(uuid, LH_id, job) VALUES (?, ?)
+            ON CONFLICT(uuid) DO UPDATE SET 
+              LH_id=excluded.LH_id,
+              job=excluded.job;
+        """, (job.id, json.dumps(asdict(job))))
+        
+        self.db.commit()
+
+    def get_job_by_uuid(self, uuid: str) -> LHJob | None:
+        """Queries database and returns job based on internal UUID. There should only
+            ever be one entry.
+
+        Args:
+            uuid (str): uuid
+
+        Returns:
+            LHJob: returned job, None if not found
+        """
+
+        res = self.db.execute(f"SELECT job FROM {self.table_name} WHERE uuid='{uuid}'")
+        results = res.fetchall()
+        return None if not len(results) else LHJob(**json.loads(results[0][0]))
+
+    def get_job_by_LH_id(self, LH_id: str) -> LHJob | None:
+        """Queries database and returns sample based on LH_id (should be unique)
+
+        Args:
+            LH_id (int): LH_id
+
+        Returns:
+            List[Sample]: returned samples, empty if not found
+        """
+        res = self.db.execute(f"SELECT job FROM {self.table_name} WHERE LH_id='{LH_id}'")
+        results = res.fetchall()
+        return None if not len(results) else LHJob(**json.loads(results[0][0]))
+    
+    def get_max_LH_id(self) -> int:
+        """Gets maximum LH_id from database"""
+
+        res = self.db.execute(f"SELECT MAX(LH_id) FROM {self.table_name}")
+        maxval = res.fetchone()
+        return maxval[0]
+
+
+class LHInterface:
+    """Basic interface for the liquid handler. Accepts only one job at a time."""
+    def __init__(self) -> None:
+        self._active_job: LHJob | None = None
+        self.running: bool = True
+
+    def update_history(self) -> None:
+        """Updates the active job in history
+        """
+
+        if self._active_job is not None:
+            with LHJobHistory() as history:
+                history.smart_insert(self._active_job)
+
+    def get_status(self) -> InterfaceStatus:
+        """Gets status of the interface"""
+
+        if not self.running:
+            return InterfaceStatus.DOWN
+
+        if self._active_job is not None:
+            return InterfaceStatus.BUSY
+
+        return InterfaceStatus.UP
+
+    def get_active_job(self) -> LHJob | None:
+        """Gets LHJob"""
+
+        return self._active_job
+
+    def update_job(self, job: LHJob):
+        """Updates the active job. Active job must exist and have
+            same id as the updated job"""
+
+        # check that we're updating the right job
+        if self._active_job is not None:
+            if job.id != self._active_job.id:
+                raise RuntimeError(f'Received update for job {job.id} but active job is {self._active_job.id}')
+        else:
+            raise RuntimeError(f'Received update for job {job.id} but no active job exists')
+    
+        self._active_job = job
+        self.update_history()
+
+    def activate_job(self, job: LHJob):
+        """Activates an LHJob"""
+
+        # check that interface is idle
+        if self.get_status() != InterfaceStatus.UP:
+
+            raise RuntimeError('Attempted to activate job but LHInterface is not idle')
+
+        with LHJobHistory() as history:
+            """get max existing ID"""
+            max_LH_id = history.get_max_LH_id()
+
+        # set to zero if no records yet
+        if max_LH_id is None:
+            max_LH_id = 0
+        
+        # assign new ID
+        job.LH_id = max_LH_id + 1
+        job.samplelist['LH_id'] = job.LH_id
+        
+        # activate job
+        self._active_job = job
+
+        self.update_history()
+        
+    def deactivate(self):
+        """Removes current job and goes idle
+        """
+
+        self.update_history()
+        self._active_job = None
+        
+lh_interface = LHInterface()
+
+if __name__ == '__main__':
+
+    with LHJobHistory() as history:
+        print(history.get_max_LH_id())
