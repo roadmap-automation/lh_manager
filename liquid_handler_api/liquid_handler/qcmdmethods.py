1564692340eba30967ec1dd6d8f04b0865f94a16
David P. Hoogerheide <david.hoogerheide@nist.gov>
2024-05-28 13:02:06 -0400

    qcmd methods parallel to injection methods

--- liquid_handler_api/liquid_handler/qcmdmethods.py
+++ liquid_handler_api/liquid_handler/qcmdmethods.py
@@ -29,7 +29,7 @@ class QCMDMeasurementDevice(DeviceBase):
     def create_job_data(method_list: List[dict]) -> dict:
         """Makes an LHJob from a list of methods"""
 
-        return {'method_data': [method_list]}
+        return {'method_list': method_list}
 
 @dataclass
 class BaseQCMDMethod(BaseMethod):
@@ -41,6 +41,8 @@ class BaseQCMDMethod(BaseMethod):
     @dataclass
     class sub_method:
         """Base class for representation in sample lists"""
+        method_name: str
+        method_data: dict = field(default_factory=dict)
 
         def to_dict(self) -> dict:
             """Creates dictionary representation; all custom field keys are prepended with a hash (#)
@@ -78,16 +80,13 @@ class QCMDInit(BaseQCMDMethod):
     display_name: Literal['QCMD Init'] = 'QCMD Init'
     method_name: Literal['QCMDInit'] = 'QCMDInit'
 
-    @dataclass
-    class sub_method(BaseQCMDMethod.sub_method):
-        pass
-
     def render_lh_method(self,
                          sample_name: str,
                          sample_description: str,
                          layout: LHBedLayout) -> List[dict]:
         
-        return [self.sub_method().to_dict()]
+        return [self.sub_method(method_name=self.method_name,
+                                method_data={}).to_dict()]
 
     def estimated_time(self, layout: LHBedLayout) -> float:
         # flow rates are not defined, so can't really do this. Need to know loop volume and aspirate and dispense flow rates
@@ -101,16 +100,14 @@ class QCMDSleep(BaseQCMDMethod):
     display_name: Literal['QCMD Sleep'] = 'QCMD Sleep'
     method_name: Literal['QCMDSleep'] = 'QCMDSleep'
 
-    @dataclass
-    class sub_method(BaseQCMDMethod.sub_method):
-        sleep_time: float
-
     def render_lh_method(self,
                          sample_name: str,
                          sample_description: str,
                          layout: LHBedLayout) -> List[dict]:
         
-        return [self.sub_method(self.sleep_time).to_dict()]
+        return [self.sub_method(method_name=self.method_name,
+                                method_data=dict(sleep_time=self.sleep_time)
+                                ).to_dict()]
 
     def estimated_time(self, layout: LHBedLayout) -> float:
         # flow rates are not defined, so can't really do this. Need to know loop volume and aspirate and dispense flow rates
@@ -125,19 +122,15 @@ class QCMDRecord(BaseQCMDMethod):
     display_name: Literal['QCMD Record'] = 'QCMD Record'
     method_name: Literal['QCMDRecord'] = 'QCMDRecord'
 
-    @dataclass
-    class sub_method(BaseQCMDMethod.sub_method):
-        record_time: float
-        sleep_time: float
-
     def render_lh_method(self,
                          sample_name: str,
                          sample_description: str,
                          layout: LHBedLayout) -> List[dict]:
         
-        return [self.sub_method(record_time=self.record_time,
-                                sleep_time=self.sleep_time).to_dict()]
-
+        return [self.sub_method(method_name=self.method_name,
+                                method_data=dict(record_time=self.record_time,
+                                                 sleep_time=self.sleep_time)
+                                ).to_dict()]
     def estimated_time(self, layout: LHBedLayout) -> float:
         # flow rates are not defined, so can't really do this. Need to know loop volume and aspirate and dispense flow rates
         return self.sleep_time + self.record_time
