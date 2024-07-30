954c49d25e7c601428cf765f28511a2290587270
bbm <brian.maranville@nist.gov>
2024-07-30 18:40:48 -0400

    adding endpoints for materials manager

--- liquid_handler_api/app.py
+++ liquid_handler_api/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, redirect
 from .gui_api import gui_blueprint
 from .lh_api import lh_blueprint
 from .nice_api import nice_blueprint
+from .material_db import blueprint as material_db_blueprint
 from .sio import socketio
 
 import mimetypes
@@ -19,6 +20,7 @@ app.config['JSON_SORT_KEYS'] = False
 app.register_blueprint(gui_blueprint)
 app.register_blueprint(nice_blueprint)
 app.register_blueprint(lh_blueprint)
+app.register_blueprint(material_db_blueprint)
 socketio.init_app(app)
 
 #@app.route('/')
