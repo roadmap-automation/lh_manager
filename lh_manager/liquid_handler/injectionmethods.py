831680126045e1f64e708dae6c980d4f72900290
David P. Hoogerheide <david.hoogerheide@nist.gov>
2025-03-04 16:26:35 -0500

    rinse system methods

--- lh_manager/liquid_handler/injectionmethods.py
+++ lh_manager/liquid_handler/injectionmethods.py
@@ -1,9 +1,10 @@
-from .bedlayout import LHBedLayout
+from .bedlayout import LHBedLayout, Composition
 from .methods import BaseMethod, MethodType, register
 from .devices import DeviceBase, device_manager
 from .job import JobBase
+from .rinsemethods import BaseRinseMethod
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from dataclasses import field
 from typing import List, Literal, ClassVar
@@ -136,3 +137,175 @@ class InjectLoop(BaseInjectionSystemMethod):
     def estimated_time(self, layout: LHBedLayout) -> float:
         return self.Volume / self.Flow_Rate
 
+# =========== Rinse system methods ============
+# NOTE: these methods use the rinse system device
+
+@register
+class RinseLoadLoop(BaseInjectionSystemMethod, BaseRinseMethod):
+    """Load injection system loop via rinse system"""
+    Rinse_Composition: Composition = Field(default_factory=Composition)
+    Aspirate_Flow_Rate: str | float = 1 # mL/min
+    Flow_Rate: str | float = 1 # mL/min
+    Volume: str | float = 1 # ml
+    Extra_Volume: str | float = 0.1 #mL
+    Air_Gap: str | float = 0.1 #ml
+    Rinse_Volume: str | float = 0.5 # ml
+    display_name: Literal['Load Injection Loop from Rinse'] = 'Load Injection Loop from Rinse'
+    method_name: Literal['RinseLoadLoop'] = 'RinseLoadLoop'
+    method_type: Literal[MethodType.INJECT] = MethodType.INJECT
+
+    def render_method(self,
+                         sample_name: str,
+                         sample_description: str,
+                         layout: LHBedLayout) -> List[BaseInjectionSystemMethod.sub_method]:
+        
+        return [
+            self.sub_method(
+                method_name='RinseLoadLoop',
+                method_data={'composition': self.Rinse_Composition,
+                             'aspirate_flow_Rate': self.Aspirate_Flow_Rate,
+                             'excess_volume': self.Extra_Volume,
+                             'air_gap': self.Air_Gap,
+                             'rinse_volume': self.Rinse_Volume,
+                             'pump_volume': self.Volume,
+                             'flow_rate': self.Flow_Rate}
+            ).to_dict() |
+            BaseRinseMethod.sub_method(method_name='InitiateRinse',
+                                       method_data={})]
+
+    def estimated_time(self, layout: LHBedLayout) -> float:
+        total_volume = self.Extra_Volume + self.Volume + 2 * self.Air_Gap
+        return 60 * (total_volume / self.Aspirate_Flow_Rate + total_volume / self.Flow_Rate + self.Rinse_Volume / self.Flow_Rate)
+    
+@register
+class RinseLoadLoopBubbleSensor(RinseLoadLoop):
+    """Inject contents of injection system loop"""
+    display_name: Literal['Load Injection Loop from Rinse with Bubble Sensor'] = 'Load Injection Loop from Rinse with Bubble Sensor'
+    method_name: Literal['RinseLoadLoopBubbleSensor'] = 'RinseLoadLoopBubbleSensor'
+
+    def render_method(self,
+                         sample_name: str,
+                         sample_description: str,
+                         layout: LHBedLayout) -> List[BaseInjectionSystemMethod.sub_method]:
+        
+        return [
+            self.sub_method(
+                method_name='RinseLoadLoopBubbleSensor',
+                method_data={'composition': self.Rinse_Composition,
+                             'aspirate_flow_Rate': self.Aspirate_Flow_Rate,
+                             'excess_volume': self.Extra_Volume,
+                             'air_gap': self.Air_Gap,
+                             'rinse_volume': self.Rinse_Volume,
+                             'pump_volume': self.Volume,
+                             'flow_rate': self.Flow_Rate}
+            ).to_dict() |
+            BaseRinseMethod.sub_method(method_name='InitiateRinse',
+                                       method_data={})]
+
+    def estimated_time(self, layout: LHBedLayout) -> float:
+        total_volume = self.Extra_Volume + self.Volume + 2 * self.Air_Gap
+        return 60 * (total_volume / self.Aspirate_Flow_Rate + total_volume / self.Flow_Rate + self.Rinse_Volume / self.Flow_Rate)
+    
+@register
+class RinseDirectInjectPrime(BaseInjectionSystemMethod, BaseRinseMethod):
+    """Prime injection system loop via rinse system"""
+    Volume: str | float = 1 # ml
+    Flow_Rate: str | float = 1 # mL/min
+    display_name: Literal['Prime Direct Injection from Rinse'] = 'Prime Direct Injection from Rinse'
+    method_name: Literal['RinseDirectInjectPrime'] = 'RinseDirectInjectPrime'
+    method_type: Literal[MethodType.NONE] = MethodType.NONE
+
+    def render_method(self,
+                         sample_name: str,
+                         sample_description: str,
+                         layout: LHBedLayout) -> List[BaseInjectionSystemMethod.sub_method]:
+        
+        return [
+            self.sub_method(
+                method_name='RinseDirectInjectPrime',
+                method_data={'pump_volume': self.Volume,
+                             'flow_rate': self.Flow_Rate}
+            ).to_dict() |
+            BaseRinseMethod.sub_method(method_name='InitiateRinse',
+                                       method_data={})]
+
+    def estimated_time(self, layout: LHBedLayout) -> float:
+        return 60 * self.Volume / self.Flow_Rate
+
+@register
+class RinseDirectInject(BaseInjectionSystemMethod, BaseRinseMethod):
+    """Direct injection via rinse system"""
+    Rinse_Composition: Composition = Field(default_factory=Composition)
+    Volume: str | float = 1 # ml
+    Extra_Volume: str | float = 0.1 #mL
+    Air_Gap: str | float = 0.1 #ml
+    Aspirate_Flow_Rate: str | float = 1 # mL/min
+    Flow_Rate: str | float = 1 # mL/min
+    Inject_Flow_Rate: str | float = 1 # mL/min
+    Rinse_Volume: str | float = 0.5 # ml
+    display_name: Literal['Direct Inject from Rinse'] = 'Direct Inject from Rinse'
+    method_name: Literal['RinseDirectInject'] = 'RinseDirectInject'
+    method_type: Literal[MethodType.INJECT] = MethodType.INJECT
+
+    def render_method(self,
+                         sample_name: str,
+                         sample_description: str,
+                         layout: LHBedLayout) -> List[BaseInjectionSystemMethod.sub_method]:
+        
+        return [
+            self.sub_method(
+                method_name='RinseDirectInject',
+                method_data={'composition': self.Rinse_Composition,
+                             'aspirate_flow_Rate': self.Aspirate_Flow_Rate,
+                             'inject_flow_rate': self.Inject_Flow_Rate,
+                             'excess_volume': self.Extra_Volume,
+                             'air_gap': self.Air_Gap,
+                             'rinse_volume': self.Rinse_Volume,
+                             'pump_volume': self.Volume,
+                             'flow_rate': self.Flow_Rate}
+            ).to_dict() |
+            BaseRinseMethod.sub_method(method_name='InitiateRinse',
+                                       method_data={})]
+
+    def estimated_time(self, layout: LHBedLayout) -> float:
+        total_volume = self.Extra_Volume + self.Volume + 2 * self.Air_Gap
+        return 60 * (total_volume / self.Aspirate_Flow_Rate + self.Volume / self.Inject_Flow_Rate + (self.Rinse_Volume + self.Extra_Volume) / self.Flow_Rate)
+    
+@register
+class RinseDirectInjectBubbleSensor(BaseInjectionSystemMethod, BaseRinseMethod):
+    """Direct injection via rinse system with bubble sensors"""
+    Rinse_Composition: Composition = Field(default_factory=Composition)
+    Volume: str | float = 1 # ml
+    Extra_Volume: str | float = 0.1 #mL
+    Air_Gap: str | float = 0.1 #ml
+    Aspirate_Flow_Rate: str | float = 1 # mL/min
+    Flow_Rate: str | float = 1 # mL/min
+    Inject_Flow_Rate: str | float = 1 # mL/min
+    Rinse_Volume: str | float = 0.5 # ml
+    display_name: Literal['Direct Inject from Rinse with Bubble Sensors'] = 'Direct Inject from Rinse with Bubble Sensors'
+    method_name: Literal['RinseDirectInjectBubbleSensor'] = 'RinseDirectInjectBubbleSensor'
+    method_type: Literal[MethodType.INJECT] = MethodType.INJECT
+
+    def render_method(self,
+                         sample_name: str,
+                         sample_description: str,
+                         layout: LHBedLayout) -> List[BaseInjectionSystemMethod.sub_method]:
+        
+        return [
+            self.sub_method(
+                method_name='RinseDirectInjectBubbleSensor',
+                method_data={'composition': self.Rinse_Composition,
+                             'aspirate_flow_Rate': self.Aspirate_Flow_Rate,
+                             'inject_flow_rate': self.Inject_Flow_Rate,
+                             'excess_volume': self.Extra_Volume,
+                             'air_gap': self.Air_Gap,
+                             'rinse_volume': self.Rinse_Volume,
+                             'pump_volume': self.Volume,
+                             'flow_rate': self.Flow_Rate}
+            ).to_dict() |
+            BaseRinseMethod.sub_method(method_name='InitiateRinse',
+                                       method_data={})]
+
+    def estimated_time(self, layout: LHBedLayout) -> float:
+        total_volume = self.Extra_Volume + self.Volume + 2 * self.Air_Gap
+        return 60 * (total_volume / self.Aspirate_Flow_Rate + self.Volume / self.Inject_Flow_Rate + (self.Rinse_Volume + self.Extra_Volume) / self.Flow_Rate)
